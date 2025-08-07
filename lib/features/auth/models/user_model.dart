2858d62cf0c2093c54d18ae9c58e37dfc2f83d41
Bipul Karki <karkibipul2@gmail.com>
2025-08-07 13:55:29 +0545

    feat(task2): google signin complete

---
+++ lib/features/auth/models/user_model.dart
@@ -0,0 +1,23 @@
+class UserModel {
+  final String uid;
+  final String name;
+  final String email;
+
+  UserModel({
+    required this.uid,
+    required this.name,
+    required this.email,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'uid': uid,
+        'name': name,
+        'email': email,
+      };
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        uid: json['uid'],
+        name: json['name'],
+        email: json['email'],
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -94,15 +94,53 @@
 
 // Task 2 Weather APP
 
-import 'package:flutter/material.dart';
-import 'package:flutter_assignment_app/features/weather/weather_provider.dart';
-import 'package:flutter_assignment_app/features/weather/screens/weather_screen.dart';
-import 'package:provider/provider.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_assignment_app/features/weather/weather_provider.dart';
+// import 'package:flutter_assignment_app/features/weather/screens/weather_screen.dart';
+// import 'package:provider/provider.dart';
 
-void main() {
+// void main() {
+//   runApp(
+//     ChangeNotifierProvider(
+//       create: (_) => WeatherProvider(),
+//       child: const MyApp(),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(home: WeatherScreen());
+//   }
+// }
+
+// Firebase auth
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'features/auth/screens/login_screen.dart';
+import 'features/weather/weather_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyA99SDd_RWFgBp_3MbIDT32w2TyoAHEcn4",
+      authDomain: "flutter-assignment-bipul.firebaseapp.com",
+      projectId: "flutter-assignment-bipul",
+      storageBucket: "flutter-assignment-bipul.firebasestorage.app",
+      messagingSenderId: "653741363027",
+      appId: "1:653741363027:web:e0e11915027cbafa8c9ba6",
+      measurementId: "G-L29CJ8JRNX",
+    ),
+  );
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => WeatherProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => WeatherProvider()),
+      ],
       child: const MyApp(),
     ),
   );
@@ -112,6 +150,11 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: WeatherScreen());
+    return MaterialApp(
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const LoginScreen(),
+    );
   }
 }
